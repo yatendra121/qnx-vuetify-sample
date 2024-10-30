2fb1390b6e479d0c30ea71b917300ca6d9adab3b
yatendra121 <yatendra121kushwaha@gmail.com>
2024-10-30 11:50:12 +0530

    fix: update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,42 +4,23 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // Utilities
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "node:url";
-import Markdown from "unplugin-vue-markdown/vite";
-import Pages from "vite-plugin-pages";
-import prism from "markdown-it-prism";
-import Inspect from "vite-plugin-inspect";
-
-//import Components from "unplugin-vue-components/vite";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      include: [/\.vue$/, /\.md$/],
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vue(),
     vuetify({
       autoImport: true,
     }),
-    Markdown({
-      headEnabled: true,
-      markdownItUses: [prism],
-    }),
-    Pages({
-      pagesDir: "pages",
-      extensions: ["vue", "md"],
-    }),
-    Inspect(),
   ],
-  // define: { "process.env": {} },
-  // resolve: {
-  //   alias: {
-  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-  //   },
-  //   extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  // },
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+  },
   build: {
     sourcemap: true,
   },
