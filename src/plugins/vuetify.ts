9e901113a431e241b6601f2b56fcfcd072eff77b
yatendra121 <yatendra121kushwaha@gmail.com>
2026-04-15 23:21:56 +0530

    fix(config): update vuetify config

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -10,9 +10,13 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { VFileUpload } from 'vuetify/labs/VFileUpload'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: {
+    VFileUpload,
+  },
   theme: {
     themes: {
       light: {
